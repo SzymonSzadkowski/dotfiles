5fcc1e4fd872b5baf8dc6b682556c4951499cf31
naqet <naqet8@gmail.com>
2024-02-05 15:01:14 +0100

    Removed old config and added karabiner

--- .config/nvim/lua/naqet/set.lua
+++ .config/nvim/lua/naqet/set.lua
@@ -9,7 +9,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.g.netrw_browse_split = 4
+vim.g.netrw_browse_split = 0
 
 vim.opt.smartindent = true
 
@@ -20,7 +20,7 @@ vim.opt.incsearch = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.termguicolors = true
--- .config/old/ftdetect/astro.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, { pattern = "*.astro", command = "set filetype=astro" })
